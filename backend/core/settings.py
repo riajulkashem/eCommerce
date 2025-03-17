472f84f37019d9464ae4ff12d404e5560fc5f15a
riajulkashem <riajul.programmer@gmail.com>
2025-03-18 03:28:42 +0600

    create app user

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
+
 import environ
 import os
 from pathlib import Path
@@ -22,19 +23,19 @@ env = environ.Env(
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Take environment variables from .env file
-environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env('DEBUG')
+DEBUG = env("DEBUG")
 
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
 
 
 # Application definition
@@ -46,6 +47,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+
+    # Local Apps
+    "user.apps.UserConfig",
 ]
 
 MIDDLEWARE = [
@@ -83,16 +87,15 @@ WSGI_APPLICATION = "core.wsgi.application"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': env.str('DATABASE_ENGINE', 'django.db.backends.postgresql'),
-        'NAME': env.str('DATABASE_NAME', 'postgres'),
-        'USER': env.str('DATABASE_USER', 'postgres'),
-        'PASSWORD': env.str('DATABASE_PASSWORD', 'postgres'),
-        'HOST': env.str('DATABASE_HOST', 'db'),
-        'PORT': env.str('DATABASE_PORT', '5432'),
+    "default": {
+        "ENGINE": env.str("DATABASE_ENGINE", "django.db.backends.postgresql"),
+        "NAME": env.str("DATABASE_NAME", "postgres"),
+        "USER": env.str("DATABASE_USER", "postgres"),
+        "PASSWORD": env.str("DATABASE_PASSWORD", "postgres"),
+        "HOST": env.str("DATABASE_HOST", "db"),
+        "PORT": env.str("DATABASE_PORT", "5432"),
     }
 }
-
 
 
 # Password validation
