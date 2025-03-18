13a173ca880b6ba04cb9c7b5ddd97b3680e2de85
riajulkashem <riajul.programmer@gmail.com>
2025-03-18 22:17:58 +0600

    add a sample html template for home page

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -18,7 +18,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from user.views import home
+
 urlpatterns = [
+    path("", home, name="home"),
     path("admin/", admin.site.urls),
     # User APP urls
     path("api/v1/", include("user.api.v1.urls")),
