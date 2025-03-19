bf5a685fae335aa4879249153d49f959bc0cf476
riajulkashem <riajul.programmer@gmail.com>
2025-03-19 16:34:39 +0600

    register models to django admin

--- backend/product/admin.py
+++ backend/product/admin.py
@@ -1,1 +1,16 @@
-# Register your models here.
+from product.models import Category, Product, Stock
+from django.contrib import admin
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'price', 'category', 'stock', 'created_at', 'updated_at', 'created_by')
+    search_fields = ('name', 'price')
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'created_at', 'updated_at', 'created_by')
+    search_fields = ('name',)
+
+@admin.register(Stock)
+class StockAdmin(admin.ModelAdmin):
+    list_display = ('id', 'product', 'quantity', 'location')
---
+++ backend/user/admin.py
@@ -0,0 +1,9 @@
+from user.models import EcommerceUser
+from django.contrib import admin
+
+
+@admin.register(EcommerceUser)
+class EcommerceUserAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 'first_name', 'last_name', 'email', 'phone', 'address', 'is_active', 'is_staff', 'is_superuser'
+    )
