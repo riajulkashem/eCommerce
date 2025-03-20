458a8332858390029d84873af37f5ad1e339a378
riajulkashem <riajul.programmer@gmail.com>
2025-03-21 04:38:16 +0600

    setup docker for frontend as well as add nginx too

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# Wait for Postgres to be ready
+/wait-for-postgres.sh postgresdb
+
 python manage.py collectstatic --noinput
 python manage.py migrate --noinput
-python -m gunicorn --bind 0.0.0.0:8000 core.wsgi:application --reload
+python -m gunicorn --bind 0.0.0.0:8000 core.wsgi:application
